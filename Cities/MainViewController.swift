b41223a024734563607ea8594dcdcc31e0165996
Vanio16 <unclevanya.339@mail.ru>
2021-08-19 15:20:26 +0600

    first commit

---
+++ Cities/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cities
+//
+//  Created by Ivan Zakharov on 19/8/21.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
