d5f063614d30d8662008e9b592dd06fbe504f219
Vanio16 <unclevanya.339@mail.ru>
2021-08-20 14:10:16 +0600

    added main and detail screens

---
+++ City/MainViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  Cities
+//
+//  Created by Ivan Zakharov on 19/8/21.
+//
+
+import UIKit
+import Framezilla
+
+protocol MainViewControllerOutput {
+    func showDetailScreen(_ image: UIImage, _ name: String)
+}
+
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private let tableView: UITableView = .init()
+    var output: MainViewControllerOutput?
+    var selectCity: City?
+    var cities = [City(name: "Omsk", picture: "omsk"),
+                  City(name: "Moscow", picture: "moscow"),
+                  City(name: "St. Petersburg", picture: "piter"),
+                  City(name: "Novosibirsk", picture: "novosib"),
+                  City(name: "Samara", picture: "samara"),
+                  City(name: "Irkutsk", picture: "irkutsk"),
+                  City(name: "Omsk", picture: "omsk"),
+                  City(name: "Moscow", picture: "moscow"),
+                  City(name: "St. Petersburg", picture: "piter"),
+                  City(name: "Novosibirsk", picture: "novosib"),
+                  City(name: "Samara", picture: "samara"),
+                  City(name: "Irkutsk", picture: "irkutsk"),
+                  City(name: "Omsk", picture: "omsk"),
+                  City(name: "Moscow", picture: "moscow"),
+                  City(name: "St. Petersburg", picture: "piter"),
+                  City(name: "Novosibirsk", picture: "novosib"),
+                  City(name: "Samara", picture: "samara"),
+                  City(name: "Irkutsk", picture: "irkutsk")]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(TableViewCell.self, forCellReuseIdentifier: "tableViewCell")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.configureFrame { maker in
+            maker.top(to: view.nui_safeArea.top)
+                .bottom(to: view.nui_safeArea.bottom)
+                .left()
+                .right()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath) as! TableViewCell
+        cell.commonInit(cities[indexPath.row].picture, title: cities[indexPath.row].name)
+        //        cell.titleLabel?.text = cities[indexPath.row].name
+        //        cell.cityImage?.image = cities[indexPath.row].picture
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 110
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectCity = cities[indexPath.row]
+        //        self.modalPresentationStyle = .fullScreen
+        //        self.present(secondViewController, animated: true, completion: nil)
+        //self.navigationController?.pushViewController(detailViewController, animated: true)
+        output?.showDetailScreen(cities[indexPath.item].picture, cities[indexPath.item].name)
+        self.tableView.deselectRow(at: indexPath, animated: true)
+        print(indexPath)
+    }
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        selectCity = cities[indexPath.row]
+        return indexPath
+    }
+}
+
+
